1ac90778ebdb19f61a0527206419065b187ef43b
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-02-25 16:10:56 -0600

    Ejercicio 3

--- Ejemplo 1/FrmRecibe.Designer.cs
+++ Ejemplo 1/FrmRecibe.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.dgvRecibe = new System.Windows.Forms.DataGridView();
             this.btnLlenar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxFiltro = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecibe)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,15 +39,15 @@
             // 
             this.dgvRecibe.AllowUserToOrderColumns = true;
             this.dgvRecibe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRecibe.Location = new System.Drawing.Point(31, 26);
+            this.dgvRecibe.Location = new System.Drawing.Point(36, 74);
             this.dgvRecibe.Name = "dgvRecibe";
             this.dgvRecibe.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvRecibe.Size = new System.Drawing.Size(737, 332);
+            this.dgvRecibe.Size = new System.Drawing.Size(737, 305);
             this.dgvRecibe.TabIndex = 0;
             // 
             // btnLlenar
             // 
-            this.btnLlenar.Location = new System.Drawing.Point(354, 388);
+            this.btnLlenar.Location = new System.Drawing.Point(359, 409);
             this.btnLlenar.Name = "btnLlenar";
             this.btnLlenar.Size = new System.Drawing.Size(90, 29);
             this.btnLlenar.TabIndex = 1;
@@ -53,17 +55,39 @@
             this.btnLlenar.UseVisualStyleBackColor = true;
             this.btnLlenar.Click += new System.EventHandler(this.btnLlenar_Click_1);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtro";
+            // 
+            // tbxFiltro
+            // 
+            this.tbxFiltro.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxFiltro.Location = new System.Drawing.Point(205, 25);
+            this.tbxFiltro.Name = "tbxFiltro";
+            this.tbxFiltro.Size = new System.Drawing.Size(360, 26);
+            this.tbxFiltro.TabIndex = 3;
+            this.tbxFiltro.TextChanged += new System.EventHandler(this.tbxFiltro_TextChanged);
+            // 
             // FrmRecibe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxFiltro);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLlenar);
             this.Controls.Add(this.dgvRecibe);
             this.Name = "FrmRecibe";
             this.Text = "FrmRecibe";
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecibe)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +95,7 @@
 
         private System.Windows.Forms.DataGridView dgvRecibe;
         private System.Windows.Forms.Button btnLlenar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxFiltro;
     }
 }
--- Ejemplo 1/FrmRecibe.cs
+++ Ejemplo 1/FrmRecibe.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Ejemplo_1
@@ -10,16 +12,70 @@ namespace Ejemplo_1
         public FrmRecibe()
         {
             InitializeComponent();
+            ConfigFilter();
         }
+
+        private void ConfigFilter()
+        {
+            tbxFiltro.GotFocus += tbxFiltro_Enter;
+            tbxFiltro.LostFocus += tbxFiltro_Leave;
+            tbxFiltro.Text = "Buscar..."; // Establece el texto inicial del placeholder
+            tbxFiltro.ForeColor = Color.Gray;
+            dgvRecibe.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvRecibe.AllowUserToOrderColumns = false;
+        }
+
         private void actualizarGrid() //función que llena el DGV del formulario 2
         {
             dgvRecibe.DataSource = null;
             dgvRecibe.DataSource = PersonaRecibe;
         }
 
+        private void tbxFiltro_Enter(object sender, EventArgs e)
+        {
+            if (tbxFiltro.Text == "Buscar...") // Considera "Buscar..." como el texto del placeholder
+            {
+                tbxFiltro.Text = "";
+                tbxFiltro.ForeColor = Color.Black; // Cambia el color del texto a negro (o cualquier color que prefieras)
+            }
+        }
+
+        private void tbxFiltro_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbxFiltro.Text))
+            {
+                tbxFiltro.Text = "Buscar...";
+                tbxFiltro.ForeColor = Color.Gray; // Cambia el color del texto a gris para el placeholder
+            }
+        }
+
+
         private void btnLlenar_Click_1(object sender, EventArgs e)
         {
             actualizarGrid();
         }
+
+        private void tbxFiltro_TextChanged(object sender, EventArgs e)
+        {
+            if (PersonaRecibe == null) return; // Asegúrate de que la lista no esté vacía
+
+            string filtro = tbxFiltro.Text.ToLower(); // Obtén el texto del filtro en minúsculas para una comparación no sensible a mayúsculas/minúsculas
+
+            if (string.IsNullOrWhiteSpace(filtro))
+            {
+                actualizarGrid(); // Si el filtro está vacío, muestra la lista completa
+            }
+            else
+            {
+                var filtrado = PersonaRecibe.Where(p => p.Nombre.ToLower().Contains(filtro)
+                                                    || p.Apellido.ToLower().Contains(filtro)
+                                                    || p.Telefono.ToLower().Contains(filtro)
+                                                    || p.Correo.ToLower().Contains(filtro)).ToList();
+
+                dgvRecibe.DataSource = null;
+                dgvRecibe.DataSource = filtrado; // Actualiza el DataGridView con la lista filtrada
+            }
+        }
+
     }
 }
